a4e6cc400a6b0500da62dff3754fede6edead37e
Andy Huang <andy.k.huang@gmail.com>
2021-02-04 23:03:25 -0800

    Give functionality for Stack to be initialized with initial values

--- W3D5/adt.rb
+++ W3D5/adt.rb
@@ -1,4 +1,21 @@
 #Exercise 1
 class Stack
-  
+  def initialize(*args)
+    @stack = [*args]
+  end
+
+  def push(el)
+    @stack.push(el)
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack[-1]
+  end
+
+
 end
+
