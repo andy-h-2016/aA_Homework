cf3054bc99fb5881b34985a422aece4db101feff
Andy Huang <andy.k.huang@gmail.com>
2021-02-23 23:35:13 -0800

    W5D5

--- W5D5/polls_app/db/migrate/20210219185407_create_questions.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuestions < ActiveRecord::Migration[5.2]
-  def change
-    create_table :questions do |t|
-      t.text :text, null: false
-      t.integer :poll_id, null: false
-      t.timestamps
-    end
-  end
-end
